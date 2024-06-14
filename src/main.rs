8bdc756c5fd83a26f9aa7a2492c841101e5b9019
basvanwesting <basvanwesting@hotmail.com>
2024-06-14 13:03:36 +0200

    Add MIT License Rename to sort_uniq_c Extract print_word_count

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ use std::{
     path::PathBuf,
 };
 
-/// Count the number of lines in a file or stdin
+/// Efficient version of "sort | uniq -c" with some output options.
+/// Output order is word, count. Sorted by descending count.
 #[derive(Parser)]
 struct Cli {
-    /// Optional output delimiter, default to human readable
+    /// Optional output delimiter, default to human readable aligned text output
     #[arg(short, long)]
     delimiter: Option<char>,
     /// The path to the file to read, use - to read from stdin (must not be a tty)
@@ -33,10 +34,23 @@ fn main() {
         word_count_in_buf_reader(BufReader::new(File::open(&file).unwrap()))
     };
 
+    print_word_count(word_count, args.delimiter);
+}
+
+fn word_count_in_buf_reader<R: BufRead>(buf_reader: R) -> HashMap<String, usize> {
+    let mut count = HashMap::new();
+    for line in buf_reader.lines() {
+        let word = line.unwrap();
+        count.entry(word).and_modify(|x| *x += 1).or_insert(1);
+    }
+    count
+}
+
+fn print_word_count(word_count: HashMap<String, usize>, delimiter: Option<char>) {
     let mut word_count_vec: Vec<(&String, &usize)> = word_count.iter().collect();
     word_count_vec.sort_by(|a, b| b.1.cmp(a.1));
 
-    match args.delimiter {
+    match delimiter {
         Some(delimiter) => {
             word_count_vec.iter().for_each(|(word, count)| {
                 println!("{}{}{}", word, delimiter, count);
@@ -66,15 +80,6 @@ fn main() {
             });
         }
     }
-}
-
-fn word_count_in_buf_reader<R: BufRead>(buf_reader: R) -> HashMap<String, usize> {
-    let mut count = HashMap::new();
-    for line in buf_reader.lines() {
-        let word = line.unwrap();
-        count.entry(word).and_modify(|x| *x += 1).or_insert(1);
-    }
-    count
 }
 
 #[cfg(test)]
