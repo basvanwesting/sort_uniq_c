745588942c4ea0e116a81b652a4efd4db7ea2940
basvanwesting <basvanwesting@hotmail.com>
2025-07-15 13:57:25 +0200

    Add support for single character count instead of word count

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,9 @@ use std::{
 /// Output order is word, count. Sorted by descending count.
 #[derive(Parser)]
 struct Cli {
+    /// Optional character to count (instead of words)
+    #[arg(short, long)]
+    character: Option<char>,
     /// Optional output delimiter, default to human readable table output
     #[arg(short, long)]
     delimiter: Option<char>,
@@ -33,16 +36,36 @@ fn main() {
             Cli::command().print_help().unwrap();
             ::std::process::exit(2);
         }
-
-        word_count_in_buf_reader(BufReader::new(stdin().lock()))
+        if let Some(character) = args.character {
+            character_count_in_buf_reader(BufReader::new(stdin().lock()), character)
+        } else {
+            word_count_in_buf_reader(BufReader::new(stdin().lock()))
+        }
+    } else if let Some(character) = args.character {
+        character_count_in_buf_reader(BufReader::new(File::open(&file).unwrap()), character)
     } else {
         word_count_in_buf_reader(BufReader::new(File::open(&file).unwrap()))
     };
 
     match args.delimiter {
-        None => print_word_count_table(word_count),
+        None => print_word_count_table(word_count, args.character),
         Some(delimiter) => print_word_count_csv(word_count, delimiter),
     }
+}
+
+fn character_count_in_buf_reader<R: BufRead>(
+    buf_reader: R,
+    character: char,
+) -> HashMap<String, usize> {
+    let mut count = HashMap::new();
+    for line in buf_reader.lines() {
+        let character_count = line.unwrap().chars().filter(|&c| c == character).count();
+        count
+            .entry(character_count.to_string())
+            .and_modify(|x| *x += 1)
+            .or_insert(1);
+    }
+    count
 }
 
 fn word_count_in_buf_reader<R: BufRead>(buf_reader: R) -> HashMap<String, usize> {
@@ -54,11 +77,17 @@ fn word_count_in_buf_reader<R: BufRead>(buf_reader: R) -> HashMap<String, usize>
     count
 }
 
-fn print_word_count_table(word_count: HashMap<String, usize>) {
+fn print_word_count_table(word_count: HashMap<String, usize>, character: Option<char>) {
     let separator = Separator::builder()
         .title(Some(HorizontalLine::default()))
         .column(Some(VerticalLine::default()))
         .build();
+
+    let word_label = if character.is_some() {
+        "Occurrences"
+    } else {
+        "Word"
+    };
 
     let table = word_count
         .into_iter()
@@ -66,7 +95,10 @@ fn print_word_count_table(word_count: HashMap<String, usize>) {
         .map(|(word, count)| vec![word.cell(), count.cell().justify(Justify::Right)])
         .table()
         .separator(separator)
-        .title(vec!["Word".cell().bold(true), "Count".cell().bold(true)])
+        .title(vec![
+            word_label.cell().bold(true),
+            "Count".cell().bold(true),
+        ])
         .bold(true);
 
     print_stdout(table).unwrap();
@@ -95,6 +127,20 @@ mod tests {
         expected.insert("word1".to_string(), 2);
         expected.insert("word2".to_string(), 3);
         expected.insert("word3".to_string(), 1);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_character_count_in_buf_reader() {
+        let data = "w;o;r;d;1\nw;o;r;d;2\nw;o;;;rd1\nwo;r;d;3\nw;o;;rd2\nw;o;r;d;2;";
+        let cursor = Cursor::new(data);
+        let result = character_count_in_buf_reader(cursor, ';');
+
+        let mut expected = HashMap::new();
+        expected.insert("4".to_string(), 3);
+        expected.insert("3".to_string(), 2);
+        expected.insert("5".to_string(), 1);
 
         assert_eq!(result, expected);
     }
